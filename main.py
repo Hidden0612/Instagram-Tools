6a4d5ee23114827f1e08bc8d2ec520100d85a53c
Mr Hidden <79319385+Hidden0612@users.noreply.github.com>
2022-04-23 21:45:52 +0430

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from .insta.instagram import Instagram
+
+insta = Instagram()
+
+USERNAME = ""
+PASSWORD = ""
+
+insta.login(USERNAME, PASSWORD) # Or Load File Session
+
+info = insta.user_information("Hidden0612")
+
+print(info)
